019d551f83cb8e747b3f761719b61fa829f469be
mcrisler <crisler.mason@gmail.com>
2025-04-03 18:14:31 +0000

    initial draft

--- PartyListProject/PartyList/Program.cs
+++ PartyListProject/PartyList/Program.cs
@@ -6,8 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter the number of the mode you would like to access: \n1. View Party List\n2. Add New Guest\n3. Edit Current Guest\nMode Selection: ");
-        string mode = Console.ReadLine();
+        string mode = AskForInput("Please enter the number of the mode you would like to access: \n1. View Party List\n2. Add New Guest\n3. Edit Current Guest\nMode Selection: ");
 
         if(mode=="1"){
             
@@ -18,8 +17,7 @@ class Program
                 Console.WriteLine(row);
             }
 
-            Console.WriteLine("\nPress ENTER to return to main menu");
-            string main_menu = Console.ReadLine();
+            string main_menu = AskForInput("\nPress ENTER to return to main menu");
             if (string.IsNullOrWhiteSpace(main_menu)){
                 
             }
@@ -32,38 +30,37 @@ class Program
 
             do{
 
-                Console.Write("Enter guest first and last name: ");
-                string guest_name = Console.ReadLine();
+                string guest_name = AskForInput("Enter guest first and last name: ");
                 if (string.IsNullOrWhiteSpace(guest_name)) guest_name = "n/a";
 
-                Console.Write("Enter guest RSVP status (yes or no): ");
-                string rsvp_status = Console.ReadLine();
+                string rsvp_status = AskForInput("Enter guest RSVP status (yes or no): ");
                 if (string.IsNullOrWhiteSpace(rsvp_status)) rsvp_status = "n/a";
 
-                Console.Write("Enter guest dietary preference (Leave blank if none): ");
-                string dietary_preference = Console.ReadLine();
+                string dietary_preference = AskForInput("Enter guest dietary preference (Leave blank if none): ");
                 if (string.IsNullOrWhiteSpace(dietary_preference)) dietary_preference = "n/a";
 
-                Console.Write("Enter guest plus-one status (yes or no): ");
-                string plus_one_status = Console.ReadLine();
+                string plus_one_status = AskForInput("Enter guest plus-one status (yes or no): ");
                 if (string.IsNullOrWhiteSpace(plus_one_status)) plus_one_status = "n/a";
 
                 string row = string.Format("{0,-21} {1,-19} {2,-27} {3,-20}", guest_name, rsvp_status, dietary_preference, plus_one_status);
 
                 File.AppendAllText("party-list-data.txt", row +Environment.NewLine);
 
-                Console.Write("\nEnter one of the following options (1 or 2):\n1. Add another guest\n2. Return to main menu\nSelection: ");
-                command = Console.ReadLine();
+                command = AskForInput("\nEnter one of the following options (1 or 2):\n1. Add another guest\n2. Return to main menu\nSelection: ");
 
             } while(command!= "2");
             
         }
 
         if (mode=="3"){
-            Console.Write("Enter guest first and last name: ");
-            string edit_guest = Console.ReadLine();
+            string edit_guest = AskForInput("Enter guest first and last name: ");
 
             var lines = File.ReadAllLines("party-list-data.txt");
         }
+    }
+
+    public static string AskForInput(string message) {
+        Console.WriteLine(message);
+        return Console.ReadLine();
     }
 }
